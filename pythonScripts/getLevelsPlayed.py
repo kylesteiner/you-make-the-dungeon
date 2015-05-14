02ba979348dccef568f89cdc87f6581294fa12c9
babakd <babakd@cs.washington.edu>
2015-05-14 14:52:20 -0700

    Added the python scripts to the repository
    The python scripts, plus a database info file and a
    readme have been added into the repository
    under the directory pythonScripts.

---
+++ pythonScripts/getLevelsPlayed.py
@@ -0,0 +1,29 @@
+# Template that you should use for your analysis script. This script loads the JSON data into a players object
+# and loops over the player
+
+import json, sys
+from datetime import datetime
+
+if len(sys.argv) < 2:
+    print "enter json file"
+    sys.exit(0)
+    
+f = open(sys.argv[1]);
+players = json.loads(f.read())
+
+print "Number of players: %d" % len(players) 
+
+for player in players: 
+    print ""
+    print "Player: %s" % player['uid']
+    print "\tTimes Played: %s" % len(player['pageloads'])
+    print "\tNumber of levels played: %d" % len(player['levels'])
+    listLevels = [0] * 8
+    for level in player['levels']:
+    	if level["dqid"] is not None:
+    		listLevels[int(float(level["qid"])) - 2] += 1
+    index = 0
+    for level in listLevels:
+   		print "level %d played %d times" % ((index + 1), level)
+   		index += 1
+    print "\tAverage levels per visit: %d" % (len(player['levels']) / len(player['pageloads']))
